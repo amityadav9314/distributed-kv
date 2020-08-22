241886b48b8ff486c3db4c030c5b83799ab46e9f
Amit kumar Yadav <Amit.Yadav@go-mmt.com>
2020-08-22 12:35:00 +0530

    Initial commit

---
+++ src/main/java/com/kv/distributedkv/DistributedKvApplication.java
@@ -0,0 +1,13 @@
+package com.kv.distributedkv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedKvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedKvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kv/distributedkv/DistributedKvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kv.distributedkv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedKvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
