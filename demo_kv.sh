1794c01710ec4d1e222350981e29debf27e83c85
Amit kumar Yadav <Amit.Yadav@go-mmt.com>
2020-08-24 21:12:07 +0530

    Misc fix

--- demo_kv.sh
+++ demo_kv.sh
@@ -14,7 +14,7 @@ echo "\t ORCHESTRATOR is successfully deployed."
 echo "Now deploying peer nodes one by one..."
 echo "Redeploying 8081..."
 java -jar -Dserver.port=8081 -Dorchestrator=127.0.0.1:8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8081.log 2>&1 &
-echo "Redeploying done of 8081"
+echo "\t Redeploying done of 8081"
 
 echo "Redeploying 8082..."
 java -jar -Dserver.port=8082 -Dorchestrator=127.0.0.1:8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8082.log 2>&1 &
