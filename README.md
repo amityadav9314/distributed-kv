43cdebc430244472325e3755ade08e6de13d2c9b
Amit kumar Yadav <Amit.Yadav@go-mmt.com>
2020-08-24 21:03:00 +0530

    Added readme file

---
+++ README.md
@@ -0,0 +1,28 @@
+# Distributed Key-Value store
+
+### System requirement
+    Java 14
+    Maven 3.5.X
+    
+### Steps to run
+    git clone git@github.com:amityadav9314/distributed-kv.git
+    mv distributed-kv /tmp
+    cd /tmp/distributed-kv
+    mvn clean install
+    ./demo_kv.sh
+    
+### To add a new key (You can use any node)
+    curl --location --request POST '127.0.0.1:8080/kv/best_movie' \
+    --header 'Content-Type: text/plain' \
+    --data-raw 'Lord of the rings'
+    
+### To get value of a key (You can use any node)
+    curl --location --request GET '127.0.0.1:8080/kv/best_movie'
+    
+### To see all data on a node
+    http://localhost:8081/kv/all-data
+    
+### To see what all nodes a particular node recognize
+    http://localhost:8084/all-nodes
+    
+
---
+++ demo_kv.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+cd /tmp/distributed-kv
+echo "Stopping server..."
+kill -9 $(lsof -ti:8081)
+kill -9 $(lsof -ti:8082)
+kill -9 $(lsof -ti:8083)
+kill -9 $(lsof -ti:8084)
+echo "Servers stopped"
+
+echo "Deploying ORCHESTRATOR on port 8080..."
+java -jar -Dserver.port=8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8080.log 2>&1 &
+echo "\t ORCHESTRATOR is successfully deployed."
+
+echo "Now deploying peer nodes one by one..."
+echo "Redeploying 8081..."
+java -jar -Dserver.port=8081 -Dorchestrator=127.0.0.1:8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8081.log 2>&1 &
+echo "Redeploying done of 8081"
+
+echo "Redeploying 8082..."
+java -jar -Dserver.port=8082 -Dorchestrator=127.0.0.1:8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8082.log 2>&1 &
+echo "\t Redeploying done of 8082"
+
+echo "Redeploying 8083..."
+java -jar -Dserver.port=8083 -Dorchestrator=127.0.0.1:8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8083.log 2>&1 &
+echo "\t Redploying done of 8083"
+
+echo "Redeploying 8084..."
+java -jar -Dserver.port=8084 -Dorchestrator=127.0.0.1:8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8084.log 2>&1 &
+echo "\t Redeploying done of 8084"
