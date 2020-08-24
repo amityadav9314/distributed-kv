81e0a199398c166bccfde2e3abc4ae3ab6d20b74
Amit kumar Yadav <Amit.Yadav@go-mmt.com>
2020-08-24 21:18:11 +0530

    Added a delay of 5 secs after orcestrator server up

--- demo_kv.sh
+++ demo_kv.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 cd /tmp/distributed-kv
 echo "Stopping server..."
+kill -9 $(lsof -ti:8080)
 kill -9 $(lsof -ti:8081)
 kill -9 $(lsof -ti:8082)
 kill -9 $(lsof -ti:8083)
 kill -9 $(lsof -ti:8084)
-echo "Servers stopped"
+echo "\t Servers stopped."
 
 echo "Deploying ORCHESTRATOR on port 8080..."
 java -jar -Dserver.port=8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8080.log 2>&1 &
+sleep 5
 echo "\t ORCHESTRATOR is successfully deployed."
 
 echo "Now deploying peer nodes one by one..."
@@ -27,3 +29,5 @@ echo "\t Redploying done of 8083"
 echo "Redeploying 8084..."
 java -jar -Dserver.port=8084 -Dorchestrator=127.0.0.1:8080 /tmp/distributed-kv/target/distributed-kv-0.0.1-SNAPSHOT.jar >/tmp/distributed_kv_8084.log 2>&1 &
 echo "\t Redeploying done of 8084"
+echo "........................................"
+echo "Distributed-kv is up and running now..."
