b33f91b0b5416cc72484fa7aef7588eb9d1b4ebe
Amit kumar Yadav <Amit.Yadav@go-mmt.com>
2020-08-29 17:25:29 +0530

    Added few test cases, will add more once I get time

--- src/test/java/com/kv/distributedkv/controllers/KVControllerTest.java
+++ src/test/java/com/kv/distributedkv/controllers/KVControllerTest.java
@@ -1,4 +1,60 @@
 package com.kv.distributedkv.controllers;
 
+import com.kv.distributedkv.dtos.KVResponse;
+import com.kv.distributedkv.dtos.ResponseStatus;
+import com.kv.distributedkv.services.KVService;
+import com.kv.distributedkv.utils.KVUtil;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(SpringExtension.class)
 public class KVControllerTest {
+
+    @InjectMocks
+    private KVController kvController;
+
+    @Mock
+    private KVService kvService;
+
+    @Test
+    public void testGetKV_when_noKeyFound() {
+        HttpServletResponse httpServletResponse = Mockito.mock(HttpServletResponse.class);
+        Mockito.when(kvService.get(Mockito.anyString())).thenReturn(failedKVResponse());
+        KVResponse kvResponse = kvController.get("dummy", httpServletResponse);
+        Assert.assertEquals(ResponseStatus.FAILED, kvResponse.getStatus());
+        Assert.assertNull(null, kvResponse.getValue());
+    }
+
+    @Test
+    public void testGetKV_when_keyIsFound() {
+        HttpServletResponse httpServletResponse = Mockito.mock(HttpServletResponse.class);
+        Mockito.when(kvService.get(Mockito.anyString())).thenReturn(successVResponse());
+        KVResponse kvResponse = kvController.get("dummy", httpServletResponse);
+        Assert.assertEquals(ResponseStatus.SUCCESS, kvResponse.getStatus());
+        Assert.assertEquals("value", kvResponse.getValue());
+        Assert.assertEquals("dummy", kvResponse.getKey());
+    }
+
+    private KVResponse failedKVResponse() {
+        return KVUtil.getErrorBaseResponse(500, new RuntimeException());
+    }
+
+    private KVResponse successVResponse() {
+        KVResponse kvResponse = new KVResponse();
+        kvResponse.setKey("dummy");
+        kvResponse.setValue("value");
+        kvResponse.setStatus(ResponseStatus.SUCCESS);
+        kvResponse.setError(null);
+        return kvResponse;
+    }
 }
